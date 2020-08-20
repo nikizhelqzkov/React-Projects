42ba14d8e2fcffae0be2a06a31bc1799e3a48b19
Nikolay Zhelyazkov <nikizhelqzkov@gmail.com>
2020-08-20 16:35:41 +0300

    testing first of react

--- project/src/App.js
+++ project/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const text = "Here is your varaible!";
+    const paragraph = <p>{"Hello , today is the day!!".toUpperCase()}</p>;
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <h3>Zdr</h3>
+        <p>{text}</p>
+        {paragraph}
+      </div>
+    );
+  }
 }
 
 export default App;
