b49d473e453de52e8aca57ea837c5f2608b9f2fe
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-01-27 22:43:29 +0200

    Init

---
+++ confusion/src/App.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import { Navbar, NavbarBrand } from "reactstrap";
+
+import "./App.css";
+import { DISHES } from './shared/dishes';
+import Menu from './components/MenuComponent';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+export default App;
