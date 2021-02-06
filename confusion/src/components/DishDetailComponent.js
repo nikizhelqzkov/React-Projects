fe1ddcad2eff1bd223b2f003676a5c42ac06306b
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-02-06 21:56:50 +0200

    adding breadcrumbs and adding dishdetail zone

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,26 +1,32 @@
 import React from "react";
-import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
-  if (dish) {
-    return (
-      <div className="col-12 col-md-5 m-1">
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
-function RenderComments({ dish }) {
-  if (dish) {
-    const comment = dish.comments.map((com) => {
+
+function RenderComments({comments}) {
+  if (comments) {
+    const comment = comments.map((com) => {
       const date = new Date(com.date);
       const dateTimeFormat = new Intl.DateTimeFormat("en", {
         year: "numeric",
@@ -28,11 +34,16 @@ function RenderComments({ dish }) {
         day: "2-digit",
       });
       return (
-        <ul key={com.id} className="list-unstyled">
-          <li>{com.comment}</li>
+        <ul className="list-unstyled">
+          <li key={com.id}>
+            <p>{com.comment}</p>
+          </li>
           <br />
           <li>
-            -- {com.author} , {dateTimeFormat.format(date)}
+            <p>
+              {" "}
+              -- {com.author} , {dateTimeFormat.format(date)}
+            </p>
           </li>
         </ul>
       );
@@ -50,9 +61,23 @@ function RenderComments({ dish }) {
 
 const DishDetail = (props) => {
   return (
-    <div className="row">
-      <RenderDish dish={props.dish} />
-      <RenderComments dish={props.dish} />
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <RenderDish dish={props.dish} />
+        <RenderComments comments={props.comments} />
+      </div>
     </div>
   );
 };
