829b60750298e2b5b5412516273ca02633a1f1fc
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-02-23 11:44:44 +0200

    adding fetching and error handling

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -9,9 +9,10 @@ import {
   BreadcrumbItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import {Loading} from "./LoadingComponent";
+import { Loading } from "./LoadingComponent";
 import { Button, Modal, ModalHeader, ModalBody, Row, Label } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { baseUrl } from "../shared/baseUrl";
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -112,7 +113,7 @@ function RenderDish({ dish }) {
   return (
     <div className="col-12 col-md-5 m-1">
       <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
