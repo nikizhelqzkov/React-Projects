43392cf57c3717da2d1f43247fe5fe50a1b9f855
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-08-04 15:47:42 +0300

    Changes

--- testProject/my-app/src/components/ButtonSection/Button.Styles.js
+++ testProject/my-app/src/components/ButtonSection/Button.Styles.js
@@ -1,17 +1,20 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.button`
-  width: 100px;
-  height: 100px;
   padding: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 0 auto;
   background-color: aqua;
-  border: 0;
+  border: 5px solid red;
   color: red;
   cursor: pointer;
+  
+  width: 33%;
+  height: 100px;
+  flex-wrap: wrap;
+
   :hover {
     opacity: 0.7;
     background-color: blueviolet;
---
+++ testProject/my-app/src/components/Header/Header.Styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div `
+    h2{
+        text-align: center;
+    }
+    .grid{
+        display: flex;
+        width: 300px;
+        height: 300px;
+        margin: 0 auto;
+        flex-wrap: wrap;
+
+
+    }
+`;
