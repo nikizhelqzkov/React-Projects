925ef7ff645b64aae0ce8e61e29b3ee569f104e7
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-09-08 16:18:24 +0300

    Finished Navbar Component Adding Ui for mobile and fixed some effects in desktop

--- PVZHELYAZKOVA/pvzhelyazkova/src/components/Navbar/Navbar.Styles.js
+++ PVZHELYAZKOVA/pvzhelyazkova/src/components/Navbar/Navbar.Styles.js
@@ -4,31 +4,137 @@ export const Wrapper = styled.nav`
   background-color: #367690;
   width: 100%;
   padding: 2rem;
-  .nav--full{
-      list-style-type: none;
-      padding-left: 0;
-      justify-content: center;
-      &__items{
-          padding: 1rem;
-          a{
-              color: #fff;
+  .nav--full {
+    list-style-type: none;
+    padding-left: 0;
+    justify-content: center;
+    &__items {
+      padding: 1rem;
+      a {
+        color: #fff;
+        font-size: 1.5rem;
+        text-decoration: none;
+        font-family: "Karla", "Helvetica Neue", helvetica, arial, sans-serif;
+        line-height: 1.5;
+        padding: 1.3rem 1.5rem;
+        transition: all 0.2s ease-in-out;
+        @media (max-width: 1200px) {
+          font-size: 1rem;
+          &:hover{
               font-size: 1.5rem;
-              text-decoration: none;
-              font-family: "Karla","Helvetica Neue",helvetica,arial,sans-serif;
-              line-height: 1.5;
-              padding: 1.3rem 1.5rem ;
-              transition: all .2s ease-in-out;
-              @media(max-width:1200px){
-                  font-size: 1rem;
-              }
-
-              &:hover{
-                background-color: #4498BA;
-              }
-              &.current{
-                  background-color: #4498BA;
-              }
           }
+        }
+
+        &:hover {
+          background-color: #4498da;
+          font-size: 2rem;
+        }
+        &.current {
+          background-color: #4498ba;
+        }
       }
+    }
+  }
+  .nav--mobile {
+    &__header {
+      width: 30%;
+      justify-content: center;
+      background-color: #fff;
+      color: #000;
+      margin: 0 auto;
+      cursor: pointer;
+      padding: 1rem;
+      text-align: center;
+      @media (max-width: 700px) {
+        width: 40%;
+      }
+      @media (max-width: 600px) {
+        width: 50%;
+      }
+      @media (max-width: 400px) {
+        width: 90%;
+      }
+      span {
+        display: inline-block;
+        position: relative;
+        width: 30px;
+        border-top: 2px solid #848484;
+        top: -3px;
+        left: 0;
+        @media (max-width: 300px) {
+          width: 20px;
+          top: -3px;
+        }
+        &::after {
+          content: "";
+          position: absolute;
+          left: 0px;
+          width: 30px;
+          border-bottom: 2px solid #848484;
+          top: 8px;
+          transition: top 0.3s;
+          transform: rotate(0deg);
+          @media (max-width: 300px) {
+            width: 20px;
+            top: 7px;
+          }
+        }
+        &::before {
+          content: "";
+          position: absolute;
+          left: 0px;
+          width: 30px;
+          border-bottom: 2px solid #848484;
+          top: -13px;
+          transition: top 0.3s;
+          transform: rotate(0deg);
+          @media (max-width: 300px) {
+            width: 20px;
+            top: -10px;
+          }
+        }
+      }
+      span.open {
+        border: none;
+        &::after {
+          position: absolute;
+          content: "";
+          top: 0;
+          transform: rotate(-45deg);
+        }
+        &::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          transform: rotate(45deg);
+        }
+      }
+    }
+    &__list{
+        list-style: none;
+        padding-left: 0;
+        font-size: 1rem;
+        text-align: center;
+        padding: 1rem 0;
+        &__items{
+            padding: 1rem;
+            a{
+                text-decoration: none;
+                font-size: 2rem;
+                color: #fff;
+                transition: .3s ease-in-out;
+                &:hover{
+                    font-size: 2.5rem;
+                    color: #fff1ad;
+                }
+                @media(max-width:350px){
+                    font-size: 1.5rem;
+                    &:hover{
+                        font-size: 2rem;
+                    }
+                }
+            }
+        }
+    }
   }
 `;
--- PVZHELYAZKOVA/pvzhelyazkova/src/components/Navbar/index.js
+++ PVZHELYAZKOVA/pvzhelyazkova/src/components/Navbar/index.js
@@ -1,31 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Wrapper } from "./Navbar.Styles";
 const Navbar = ({ section }) => {
   const currentActiveClass = (text) => {
     return section === text ? "current" : "";
   };
+  const [toggle, isToggle] = useState(false);
   return (
     <Wrapper>
       <ul className="d-none d-lg-flex nav--full">
         <li className="nav--full__items">
-          <NavLink to="/" className={currentActiveClass('home')}>
+          <NavLink to="/" className={currentActiveClass("home")}>
             Начало
           </NavLink>
         </li>
         <li className="nav--full__items">
-          <NavLink to="/autobiography" className={currentActiveClass('autobiography')}>Автобиография</NavLink>
+          <NavLink
+            to="/autobiography"
+            className={currentActiveClass("autobiography")}
+          >
+            Автобиография
+          </NavLink>
         </li>
         <li className="nav--full__items">
-          <NavLink to="/qualification" className={currentActiveClass('qualification')}>Квалификация</NavLink>
+          <NavLink
+            to="/qualification"
+            className={currentActiveClass("qualification")}
+          >
+            Квалификация
+          </NavLink>
         </li>
         <li className="nav--full__items">
-          <NavLink to="/activity" className={currentActiveClass('activity')}>Дейности</NavLink>
+          <NavLink to="/activity" className={currentActiveClass("activity")}>
+            Дейности
+          </NavLink>
         </li>
         <li className="nav--full__items">
-          <NavLink to="/contacts" className={currentActiveClass('contacts')}>Контакти</NavLink>
+          <NavLink to="/contacts" className={currentActiveClass("contacts")}>
+            Контакти
+          </NavLink>
         </li>
       </ul>
+      <div className="nav--mobile d-lg-none">
+        <div
+          className="nav--mobile__header row"
+          onClick={() => isToggle(!toggle)}
+        >
+          <div className="col-6">
+            <h4>Меню</h4>
+          </div>
+          <div className="col-6">
+            {toggle ? <span className="open"></span> : <span></span>}
+          </div>
+        </div>
+        {toggle && (
+          <ul className="d-lg-none nav--mobile__list">
+            <li className="nav--mobile__list__items">
+              <NavLink to="/" className={currentActiveClass("home")}>
+                Начало
+              </NavLink>
+            </li>
+            <li className="nav--mobile__list__items">
+              <NavLink
+                to="/autobiography"
+                className={currentActiveClass("autobiography")}
+              >
+                Автобиография
+              </NavLink>
+            </li>
+            <li className="nav--mobile__list__items">
+              <NavLink
+                to="/qualification"
+                className={currentActiveClass("qualification")}
+              >
+                Квалификация
+              </NavLink>
+            </li>
+            <li className="nav--mobile__list__items">
+              <NavLink
+                to="/activity"
+                className={currentActiveClass("activity")}
+              >
+                Дейности
+              </NavLink>
+            </li>
+            <li className="nav--mobile__list__items">
+              <NavLink
+                to="/contacts"
+                className={currentActiveClass("contacts")}
+              >
+                Контакти
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
     </Wrapper>
   );
 };
